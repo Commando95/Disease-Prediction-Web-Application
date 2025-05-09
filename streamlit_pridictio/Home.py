4cccf523897f8647de0117809d0f335ea87a6ed0
Piyush Chittauria <129655939+Commando95@users.noreply.github.com>
2025-05-09 17:24:39 +0530

    Updated Home.py

--- streamlit_pridictio/Home.py
+++ streamlit_pridictio/Home.py
@@ -28,7 +28,7 @@ def main():
 
 
         contact_form = """
-        <form action="https://formsubmit.co/" method="POST">
+        <form action="" method="POST">
             <input type="hidden" name="_captcha" value="false">
             <input type="text" name="name" placeholder="Your name" required>
             <input type="email" name="email" placeholder="Your email" required>
